d0c09e719504f7c5883495cfd4ab2fc47f0e2f7b
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-15 10:51:30 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan31-PerkenalanMahasiswa/src/pbo2/pkg10116417/latihan31/perkenalanmahasiswa/PBO210116417Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author 
+ *  NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM MENAPILKAN DATA UNTUK MENGENALKAN BEBERAPA SISWA
+ * 
+ */
+public class PBO210116417Latihan31PerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.setNim("10110269");
+        mhs1.setNama("Rizki Adam Kurniawan");
+        mhs1.perkenalkanDiri();
+
+        Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.setNim("10110270");
+        mhs2.setNama("Indra Tiola");
+        mhs2.perkenalkanDiri();
+
+        Mahasiswa mhs3 = new Mahasiswa();
+        mhs3.setNim("10110271");
+        mhs3.setNama("Robi Tanzil Ganefi");
+        mhs3.perkenalkanDiri();
+
+        Mahasiswa mhs4 = new Mahasiswa();
+        mhs4.setNim("10110272");
+        mhs4.setNama("Muhammad Nur Awaluddin");
+        mhs4.perkenalkanDiri();
+
+    }
+    
+}
